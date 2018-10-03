69f756e3676a3d091439b8abd0d0b1ee0eea73cd
kingpes <kingpes#gmail.com>
2018-10-03 16:41:17 +0700

    Commit

---
+++ app/src/main/java/phuthotech/vn/flatmultiapi/Const.java
@@ -0,0 +1,14 @@
+package phuthotech.vn.flatmultiapi;
+
+/**
+ * Created by ravi on 01/03/18.
+ */
+
+public class Const {
+    // To fetch the tickets
+    // https://api.androidhive.info/json/airline-tickets.php
+
+    // To fetch individual ticket price
+    // https://api.androidhive.info/json/airline-tickets-price.php
+    public static final String BASE_URL = "https://api.androidhive.info/json/";
+}
